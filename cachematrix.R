33f155deef88344b80dc6ad58c1db678894ae445
Vishwanath Iyer <vishwanath_i@yahoo.com>
2014-05-25 21:48:56 +0530

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a programming assignment as part of the R coourse which involves creating 2 functions
+## These functions will help save computations in a cache, so they can be accessed multiple times
+## without evaluating every time, thereby saving system memory
 
-## Write a short comment describing this function
+
+## The first function, makeVector creates a special "matrix", which is really a list containing a function to
+## set the value of the matrix (if not already in cache)
+## get the value of the matrix (if already in cache)
+## set the inverse of the matrix (if not already in cache)
+## get the inverse of the matrix (if already in cache)
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+    x <<- y
+    m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix if it is not already existing in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
